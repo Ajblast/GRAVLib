31f8a46e8eaf2816c3d81b330672eb853e58d6af
Ajblast <46971018+Ajblast@users.noreply.github.com>
2022-03-19 22:17:56 -0400

    Fiber and Thread Tests

--- GRAVLib/tests/Concurrency/Fibers/FiberTests.cpp
+++ GRAVLib/tests/Concurrency/Fibers/FiberTests.cpp
@@ -1,62 +1,84 @@
-// TODO: Fiber Tests
+#include <gtest/gtest.h>
+#include "GRAVLibCore.h"
+#include "Concurrency/Fibers/Fiber.h"
 
-// Can initialze from current thread
-// Can be created
-// Can switch to other fiber
-// Can set name
+namespace GRAVLib::Concurrency::Fibers
+{
+	TEST(Fibers, CanBeInitializedFromCurrentThread)
+	{
+		fiber sut;
+		sut.initializeFromCurrentThread();
+
+		ASSERT_TRUE(sut.valid()) << "Fiber initialized from current thread was not valid.";
+
+		// Have to turn the tread back into
+		sut.convertToThread();
+
+		EXPECT_FALSE(sut.valid()) << "Fiber initialized from current thread was still valid after converting back to thread.";
+	}
+	TEST(Fibers, CanBeCreated)
+	{
+		fiber sut = fiber([](fiber*) {});
 
 
-//void GRAVCore::Jobs::fiber::canInitializeFromCurrentThread()
-//{
-//	GRAVEngine::Jobs::fiber sut;
-//	sut.initializeFromCurrentThread();
-//
-//	bool validInitialization = sut.isValid();
-//
-//	// Have to turn the tread back into
-//	sut.convertToThread();
-//
-//	bool validConversion = sut.isValid() == false;
-//
-//	Assert::IsTrue(validInitialization && validConversion);
-//}
-//
-//void GRAVCore::Jobs::fiber::canBeCreated()
-//{
-//	GRAVEngine::Jobs::fiber sut;
-//	sut.spawn([](GRAVEngine::Jobs::fiber*) {});
-//
-//	Assert::IsTrue(sut.isValid());
-//}
-//
-//static GRAVEngine::Jobs::fiber* sutPTR = nullptr;
-//static bool callbackRan = false;
-//static void testCallback(GRAVEngine::Jobs::fiber* fiber)
-//{
-//	// The callback was ran.
-//	callbackRan = true;
-//	// Switch back to the fiber
-//	fiber->switchTo(sutPTR);
-//}
-//
-//void GRAVCore::Jobs::fiber::canSwithToOtherFiber()
-//{
-//	GRAVEngine::Jobs::fiber sut;
-//	// Initialize the current fiber so switching can occur
-//	sut.initializeFromCurrentThread();
-//	sutPTR = &sut;
-//
-//	callbackRan = false;
-//
-//	GRAVEngine::Jobs::fiber sut2;
-//	sut2.spawn(testCallback);
-//
-//	// Call the other sut
-//	sut.switchTo(&sut2);
-//
-//	// Turn fiber back to thread for cleaning things up
-//	sut.convertToThread();
-//
-//	// Both fibers must be valid and the callback needs to run
-//	Assert::IsTrue(callbackRan);
-//}
+		// Close the created fiber
+		sut.close();
+	}
+
+
+	static fiber* sutPTR = nullptr;
+	static bool callbackRan = false;
+	static void testCallback(fiber* fiber)
+	{
+		// The callback was ran.
+		callbackRan = true;
+		// Switch back to the fiber
+		fiber->switchTo(*sutPTR);
+	}
+
+	TEST(Fibers, CanSwitchToOtherFibers)
+	{
+		// Initialize the current fiber so switching can occur
+		fiber sut;
+		sut.initializeFromCurrentThread();
+		sutPTR = &sut;
+		callbackRan = false;
+
+		ASSERT_TRUE(sut.valid()) << "The starting fiber is not valid.";
+
+		// Create the second fiber
+		fiber sut2 = fiber(&testCallback);
+		ASSERT_TRUE(sut2.valid()) << "The second fiber is not valid.";
+
+		// Call the other sut
+		sut.switchTo(sut2);
+
+		// Turn fiber back to thread for cleaning things up
+		sut.convertToThread();
+		// Close the second fiber
+		sut2.close();
+
+
+		EXPECT_FALSE(sut.valid()) << "Starting fiber is still valid after converting to thread.";
+		EXPECT_FALSE(sut2.valid()) << "Second fiber is still valid after closing.";
+		EXPECT_TRUE(callbackRan) << "Callback function for fiber was not run.";
+	}
+	TEST(Fibers, CanSetName)
+	{
+		std::string testName = "TestName";
+		fiber sut;
+
+		sut.setName(testName);
+
+		EXPECT_STREQ(sut.name().c_str(), testName.c_str()) << "Fiber name was not set.";
+	}
+	TEST(Fibers, CanGetCurrentFiber)
+	{
+		fiber sut;
+		sut.initializeFromCurrentThread();
+
+		fiberID id = fiber::getCurrenterFiberID();
+		EXPECT_EQ(sut.getID(), id) << "Current fiber ID does not match the created fiber ID.";
+		sut.convertToThread();
+	}
+}
--- GRAVLib/tests/Concurrency/Threads/ThreadTests.cpp
+++ GRAVLib/tests/Concurrency/Threads/ThreadTests.cpp
@@ -1,71 +1,172 @@
 #include <gtest/gtest.h>
 #include "GRAVLibCore.h"
+#include "Concurrency/Threads/Thread.h"
 
 namespace GRAVLib::Concurrency::Threads
 {
+	static bool threadFunctionValue = false;
+	unsigned int threadFunction(thread* thread)
+	{
+		threadFunctionValue = true;
+		return 0;
+	}
 
+	TEST(Threads, CanBeInitializedFromCurrentThread)
+	{
+		thread sut;
+		sut.initializeFromCurrentThread();
+		
+		// Check that the thread was valid
+		ASSERT_TRUE(sut.valid()) << "Initialzed thread was not valid.";
+
+		#if GRAVLib_PLATFORM == GRAVLib_PLATFORM_WINDOWS
+		EXPECT_NE(sut.getThreadHandle(), nullptr) << "Windows thread native handle was not initialized.";
+		#endif
+
+		sut.close();
+	}
+	TEST(Threads, CanBeCreatedWithFreeFunction)
+	{
+		ASSERT_FALSE(threadFunctionValue) << "Thread function value was not initially false.";
+
+		thread::threadCallback callbackFunction = threadFunction;
+		thread sut(callbackFunction);
+
+
+		// Check that the thread was valid
+		ASSERT_TRUE(sut.valid()) << "Initialzed thread was not valid.";
+
+		#if GRAVLib_PLATFORM == GRAVLib_PLATFORM_WINDOWS
+		EXPECT_NE(sut.getThreadHandle(), nullptr) << "Windows thread native handle was not initialized.";
+		#endif
+
+		// Wait for the thread to finish
+		sut.join();
+
+		// The value should have changed
+		EXPECT_TRUE(threadFunctionValue);
+	}
+	TEST(Threads, CanGetCurrentThread)
+	{
+		threadID id = thread::getCurrentThreadID();
+		EXPECT_NE(id, threadID()) << "Current thread ID was null";
+
+		thread sut;
+		sut.initializeFromCurrentThread();
+
+		EXPECT_EQ(id.m_ThreadID, sut.getThreadID()) << "Thread initialized from the current thread does not have the found current thread ID";
+		sut.close();
+	}
+	
+	static bool lambdaFunctionValue = false;
+	TEST(Threads, CanBeCreatedWithLambda)
+	{
+		thread::threadCallback callbackFunction = [](thread*) { lambdaFunctionValue = true; return (unsigned int)0; };
+		thread sut(callbackFunction);
+
+
+		// Check that the thread was valid
+		ASSERT_TRUE(sut.valid()) << "Initialzed thread was not valid.";
+
+		#if GRAVLib_PLATFORM == GRAVLib_PLATFORM_WINDOWS
+		EXPECT_NE(sut.getThreadHandle(), nullptr) << "Windows thread native handle was not initialized.";
+		#endif
+
+		// Wait for the thread to finish
+		sut.join();
+
+		// The value should have changed
+		EXPECT_TRUE(lambdaFunctionValue);
+	}
+
+	static bool affinityFunctionValue = false;
+	TEST(Threads, CanSetAffinity)
+	{
+		affinityFunctionValue = true;
+		thread sut([](thread*) {while (affinityFunctionValue) {} return (unsigned int) 0; });
+
+		// Check that the thread was valid
+		ASSERT_TRUE(sut.valid()) << "Initialzed thread was not valid.";
+
+		#if GRAVLib_PLATFORM == GRAVLib_PLATFORM_WINDOWS
+		EXPECT_NE(sut.getThreadHandle(), nullptr) << "Windows thread native handle was not initialized.";
+		#endif
+
+		// Set the affinity
+		size_t expectedAffinity = 1;
+		sut.setAffinity(expectedAffinity);
+
+		EXPECT_EQ(sut.getAffinity(), expectedAffinity);
+
+		// End the loop
+		affinityFunctionValue = false;
+		sut.join();
+	}
+
+	static bool nameFunctionValue = false;
+	TEST(Threads, CanSetName)
+	{
+		nameFunctionValue = true;
+		thread sut([](thread*) {while (nameFunctionValue) {} return (unsigned int)0; });
+
+		// Check that the thread was valid
+		ASSERT_TRUE(sut.valid()) << "Initialzed thread was not valid.";
+
+		#if GRAVLib_PLATFORM == GRAVLib_PLATFORM_WINDOWS
+		EXPECT_NE(sut.getThreadHandle(), nullptr) << "Windows thread native handle was not initialized.";
+		#endif
+
+		// Set the affinity
+		std::wstring expectedName = L"Test Name";
+		sut.setName(expectedName);
+
+		EXPECT_STREQ(sut.getName().c_str(), expectedName.c_str());
+
+		// End the loop
+		nameFunctionValue = false;
+		sut.join();
+	}
+
+	static bool moveFunctionValue = false;
+	TEST(Threads, CanBeMoveConstructed)
+	{
+		moveFunctionValue = true;
+		thread thread1 = thread([](thread*) {while (moveFunctionValue) {} return (unsigned int)0; });
+		thread sut = std::move(thread1);
+
+		// Check that the thread was valid
+		ASSERT_TRUE(sut.valid()) << "Initialzed thread was not valid.";
+
+		#if GRAVLib_PLATFORM == GRAVLib_PLATFORM_WINDOWS
+		EXPECT_NE(sut.getThreadHandle(), nullptr) << "Windows thread native handle was not initialized.";
+		#endif
+
+		moveFunctionValue = false;
+		sut.join();
+	}
+
+	static bool detachFunctionValue = false;
+	TEST(Threads, CanBeDetached)
+	{
+		thread sut([](thread*) {return (unsigned int)0; });
+
+		// Check that the thread was valid
+		ASSERT_TRUE(sut.valid()) << "Initialzed thread was not valid.";
+
+		#if GRAVLib_PLATFORM == GRAVLib_PLATFORM_WINDOWS
+		EXPECT_NE(sut.getThreadHandle(), nullptr) << "Windows thread native handle was not initialized.";
+		#endif
+		
+		// Detach the thread
+		sut.detach();
+
+
+		// Check that the thread was valid
+		EXPECT_FALSE(sut.valid()) << "Initialzed thread was not valid.";
+
+		#if GRAVLib_PLATFORM == GRAVLib_PLATFORM_WINDOWS
+		EXPECT_EQ(sut.getThreadHandle(), nullptr) << "Windows thread native handle was not initialized.";
+		#endif
+
+	}
 }
-
-
-// TODO: Thread Tests
-
-
-// Test the formatting of a thread ID
-
-// Can initialze from current thread
-// Can be created
-// Can set affinity
-// Can set name
-
-
-// Following code is old tests. Base tests on it
-
-//void GRAVCore::Jobs::gravThread::canInitializeFromCurrentThread()
-//{
-//	GRAVEngine::Jobs::gravThread sut;
-//
-//	sut.initializeFromCurrentThread();
-//
-//	Assert::IsTrue(sut.isValid());
-//}
-//
-//void GRAVCore::Jobs::gravThread::canBeCreated()
-//{
-//	GRAVEngine::Jobs::gravThread sut;
-//
-//	sut.spawn([](GRAVEngine::Jobs::gravThread*) {});
-//
-//	Assert::IsTrue(sut.isValid());
-//}
-//
-//static bool continueWhileThread = true;
-//void GRAVCore::Jobs::gravThread::canSetAffinity()
-//{
-//	GRAVEngine::Jobs::gravThread sut;
-//
-//	// Create a stupid thread
-//	sut.spawn([](GRAVEngine::Jobs::gravThread*) { while (continueWhileThread) {}});
-//
-//	size_t affinity = 1;
-//	sut.setAffinity(affinity);
-//
-//	size_t threadAffinity = sut.getAffinity();
-//
-//	continueWhileThread = false;
-//	sut.join();
-//
-//	Assert::IsTrue(sut.isValid() && threadAffinity == affinity);
-//}
-//
-//void GRAVCore::Jobs::gravThread::canSetName()
-//{
-//	GRAVEngine::Jobs::gravThread sut;
-//
-//	sut.initializeFromCurrentThread();
-//	std::wstring name = L"Test Thread Name";
-//	sut.setName(name);
-//
-//	std::wstring threadName = sut.getName();
-//	bool compare = threadName.compare(name) == 0;
-//	Assert::IsTrue(sut.isValid() && compare);
-//}
